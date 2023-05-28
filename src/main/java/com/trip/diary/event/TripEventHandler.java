99411aea492ea0dcc0ab164c5fc848deafe98526
haden <80521474+thdefn@users.noreply.github.com>
2023-05-28 15:43:29 +0900

    refactor : elastic search 업데이트 및 조회 쿼리를 별도의 서비스로 분리

--- src/main/java/com/trip/diary/event/TripEventHandler.java
+++ src/main/java/com/trip/diary/event/TripEventHandler.java
@@ -1,60 +1,29 @@
 package com.trip.diary.event;
 
-import com.trip.diary.elasticsearch.repository.MemberSearchRepository;
 import com.trip.diary.event.dto.TripInviteEvent;
 import com.trip.diary.event.dto.TripKickOutEvent;
+import com.trip.diary.service.MemberSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 @RequiredArgsConstructor
 public class TripEventHandler {
 
-    private final MemberSearchRepository memberSearchRepository;
-    private final ElasticsearchOperations elasticsearchOperations;
+    private final MemberSearchService memberSearchService;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void addTripIdToMemberDocument(TripInviteEvent event) {
-        List<UpdateQuery> updateQueries = memberSearchRepository.findByIdIn(event.getParticipantsIds())
-                .stream().map(
-                        memberDocument ->
-                        {
-                            memberDocument.addTripId(event.getTripId());
-                            return UpdateQuery.builder(memberDocument.getId().toString())
-                                    .withDocument(elasticsearchOperations.getElasticsearchConverter().mapObject(memberDocument))
-                                    .withDocAsUpsert(true)
-                                    .build();
-                        }
-                ).collect(Collectors.toList());
-
-        elasticsearchOperations.bulkUpdate(updateQueries, IndexCoordinates.of("members"));
+    public void handleTripInviteEvent(TripInviteEvent event) {
+        memberSearchService.addTripIdToMemberDocument(event.getParticipantsIds(), event.getTripId());
     }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void removeTripIdToMemberDocument(TripKickOutEvent event) {
-        List<UpdateQuery> updateQueries = memberSearchRepository.findById(event.getMemberId())
-                .stream().map(
-                        memberDocument ->
-                        {
-                            memberDocument.removeTripId(event.getTripId());
-                            return UpdateQuery.builder(memberDocument.getId().toString())
-                                    .withDocument(elasticsearchOperations.getElasticsearchConverter().mapObject(memberDocument))
-                                    .withDocAsUpsert(true)
-                                    .build();
-                        }
-                ).collect(Collectors.toList());
-
-        elasticsearchOperations.bulkUpdate(updateQueries, IndexCoordinates.of("members"));
+    public void handleTripKickOutEvent(TripKickOutEvent event) {
+        memberSearchService.removeTripIdToMemberDocument(event.getMemberId(), event.getTripId());
     }
 }
---
+++ src/test/java/com/trip/diary/service/MemberSearchServiceTest.java
@@ -0,0 +1,119 @@
+package com.trip.diary.service;
+
+import com.trip.diary.domain.model.Member;
+import com.trip.diary.domain.repository.MemberRepository;
+import com.trip.diary.dto.MemberDto;
+import com.trip.diary.elasticsearch.model.MemberDocument;
+import com.trip.diary.elasticsearch.repository.MemberSearchRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MemberSearchServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private MemberSearchRepository memberSearchRepository;
+    @InjectMocks
+    private MemberSearchService memberSearchService;
+
+    Member member = Member.builder()
+            .id(1L)
+            .username("qwerty99")
+            .nickname("김맹맹")
+            .password("1234567")
+            .profilePath(null)
+            .phone("01011111111")
+            .build();
+
+    @Test
+    @DisplayName("여행 기록장에 초대할 멤버 검색 성공")
+    void searchAddableMembersTest_success() {
+        //given
+        given(memberSearchRepository.findByNicknameContainsIgnoreCase(anyString()))
+                .willReturn(Arrays.asList(
+                        MemberDocument.builder()
+                                .id(1L)
+                                .nickname("바밤바")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(new MemberDocument.Trip(1L)))
+                                .build(),
+                        MemberDocument.builder()
+                                .id(3L)
+                                .nickname("투움바 파스타")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(
+                                        new MemberDocument.Trip(2L),
+                                        new MemberDocument.Trip(4L)
+                                ))
+                                .build(),
+                        MemberDocument.builder()
+                                .id(4L)
+                                .nickname("바나나")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(new MemberDocument.Trip(1L)))
+                                .build()
+                ));
+        //when
+        List<MemberDto> result = memberSearchService.searchAddableMembers("바", member);
+        //then
+        assertEquals(2, result.size());
+        assertEquals(result.get(0).getId(), 3L);
+        assertEquals(result.get(0).getNickname(), "투움바 파스타");
+        assertEquals(result.get(1).getId(), 4L);
+        assertEquals(result.get(1).getNickname(), "바나나");
+    }
+
+    @Test
+    @DisplayName("생성된 여행 기록장에 초대할 멤버 검색 성공")
+    void searchAddableMembersInTripTest_success() {
+        //given
+        given(memberSearchRepository.findByNicknameContainsIgnoreCase(anyString()))
+                .willReturn(Arrays.asList(
+                        MemberDocument.builder()
+                                .id(1L)
+                                .nickname("바밤바")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(new MemberDocument.Trip(1L)))
+                                .build(),
+                        MemberDocument.builder()
+                                .id(3L)
+                                .nickname("투움바 파스타")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(
+                                        new MemberDocument.Trip(2L),
+                                        new MemberDocument.Trip(4L)
+                                ))
+                                .build(),
+                        MemberDocument.builder()
+                                .id(4L)
+                                .nickname("바나나")
+                                .profileUrl("basic.jpg")
+                                .trips(List.of(new MemberDocument.Trip(1L)))
+                                .build()
+                ));
+        //when
+        List<MemberDto> result = memberSearchService.searchAddableMembersInTrip(1L, "바", member);
+        //then
+        assertEquals(2, result.size());
+        assertFalse(result.get(0).getIsInvited());
+        assertTrue(result.get(1).getIsInvited());
+        assertEquals(result.get(0).getId(), 3L);
+        assertEquals(result.get(0).getNickname(), "투움바 파스타");
+        assertEquals(result.get(1).getId(), 4L);
+        assertEquals(result.get(1).getNickname(), "바나나");
+    }
+
+}
